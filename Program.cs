49d9e8096546152bb3bbd990c0d503e55b3696ce
Coryn Tyrrell <103038198@student.swin.edu.au>
2020-06-05 11:04:31 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bingoNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
